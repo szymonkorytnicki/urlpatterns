7459a858fa053b171d018ce9ce954435e96f5c04
Szymon Korytnicki <szymon.korytnicki@gmail.com>
2022-08-25 23:35:02 +0200

    #1 install next

---
+++ pages/pattern.js
@@ -0,0 +1,14 @@
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>URLPatterns</title>
+        <meta name="description" content="URL Patterns" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      general kenobi
+    </div>
+  );
+}
