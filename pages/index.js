2a96e7b732635db9a6ade99d47e73a70dc83cbcc
Szymon Korytnicki <szymon.korytnicki@gmail.com>
2022-09-08 22:54:55 +0200

    NONE add ability to search and some styling

--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,58 @@
 import Head from "next/head";
 import styles from "../styles/Home.module.css";
 import { getAllPatterns } from "../getAllPatterns";
+import { useState } from "react";
+import { useRouter } from "next/router";
+import Link from "next/link";
 export default function Home({ patterns }) {
+  const router = useRouter();
+
+  function onSubmit(event) {
+    event.preventDefault();
+
+    const url = new URL(window.location);
+    url.searchParams.set("search", event.target[0].value);
+
+    router.push(url);
+  }
+
+  const foundPatterns = patterns.filter(({ title }) => {
+    if (!router.query.search) {
+      return true;
+    }
+    return title.toLowerCase().includes(router.query.search?.toLowerCase());
+  });
+
   return (
-    <div className={styles.container}>
+    <>
       <Head>
         <title>URLPatterns</title>
         <meta name="description" content="URL Patterns" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      general kenobi
-      {patterns.map((pattern) => {
-        return (
-          <div key={pattern.id}>
-            <a href={`/pattern/${pattern.id}`}>{pattern.title}</a>
-          </div>
-        );
-      })}
-    </div>
+      <header className="header">
+        <div className="wrapper">
+          <h1 className={styles.title}>URL Patterns</h1>
+          <form onSubmit={onSubmit}>
+            <input type="search" placeholder="Search here" className={styles.search} />
+            <button>Search</button>
+          </form>
+        </div>
+      </header>
+
+      <div className="wrapper">
+        {foundPatterns.length} patterns available
+        <div className={styles.patterns}>
+          {foundPatterns.slice(0, 50).map((pattern) => {
+            return (
+              <Link href={`/pattern/${pattern.id}`} className={styles.patternBox} key={pattern.id}>
+                {pattern.title}
+              </Link>
+            );
+          })}
+        </div>
+      </div>
+    </>
   );
 }
 
